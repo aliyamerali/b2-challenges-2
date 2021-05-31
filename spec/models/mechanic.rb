20fc1888c201b3c9779cf67260a15db451bdc5e3
Aliya Merali <merali4@gmail.com>
2021-05-31 12:37:25 -0600

    Setup database, add relationships tests

---
+++ spec/models/mechanic.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Mechanic do
+  describe 'relationships' do
+    it {should have_many :assignments}
+    it {should have_many(:rides).through(:assignments)}
+  end
+end
---
+++ spec/models/ride.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Ride do
+  describe 'relationships' do
+    it {should have_many :assignments}
+    it {should have_many(:mechanics).through(:assignments)}
+  end
+end
