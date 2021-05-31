3eebd530491699ecc33b59732943d5a084c76bf5
Aliya Merali <merali4@gmail.com>
2021-05-31 12:56:49 -0600

    Add model test and method, route, controller, views to pass us1

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,4 +1,8 @@
 class Mechanic < ApplicationRecord
   has_many :assignments
   has_many :rides, through: :assignments
+
+  def self.avg_yrs_exp
+    average(:years_experience)
+  end
 end
--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe Mechanic do
     it {should have_many :assignments}
     it {should have_many(:rides).through(:assignments)}
   end
+
+  describe 'class methods' do
+    it '.avg_yrs_exp returns average years experience of all mechanics' do
+      Mechanic.create!(name: "Salty Sal", years_experience: 12)
+      Mechanic.create!(name: "Tough Tony", years_experience: 23)
+      Mechanic.create!(name: "Lucky Lucy", years_experience: 8)
+
+      expect(Mechanic.avg_yrs_exp.floor).to eq(14)
+    end
+  end
 end
